bd7c2ed9d66546c3783d94e7a9ba24423026b46f
Glenn Dirkx <glenn.dirkx@gmail.com>
2024-02-09 13:25:20 +0100

    USB works again

--- code/controlplane/src/main.rs
+++ code/controlplane/src/main.rs
@@ -16,20 +16,19 @@ use embassy_executor::{Executor, InterruptExecutor, Spawner};
 use embassy_stm32::exti::ExtiInput;
 use embassy_stm32::gpio::{Input, Pin};
 use embassy_stm32::interrupt::{InterruptExt, Priority};
-// use embassy_stm32::peripherals;
-use embassy_stm32::rcc::{AHBPrescaler, APBPrescaler, Hse, Pll, PllPreDiv, PllSource, Sysclk};
+use embassy_stm32::peripherals;
 use embassy_stm32::time::Hertz;
-// use embassy_stm32::usb_otg::{self, Driver, Instance};
+use embassy_stm32::usb_otg::{self, Driver, Instance};
 use embassy_stm32::{bind_interrupts, interrupt, pac, Config as STM32Config};
 use embassy_stm32::{
     gpio::{Level, Output, Pull, Speed},
     peripherals::*,
 };
 use embassy_time::Timer;
-// use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
-// use embassy_usb::driver::EndpointError;
-// use embassy_usb::Builder;
-// use futures::future::join;
+use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
+use embassy_usb::driver::EndpointError;
+use embassy_usb::Builder;
+use futures::future::join;
 use static_cell::StaticCell;
 
 use {defmt_rtt as _, panic_probe as _};
@@ -44,13 +43,14 @@ unsafe fn USART2() {
     EXECUTOR_BLINK.on_interrupt()
 }
 
-// bind_interrupts!(struct Irqs {
-//     OTG_FS => usb_otg::InterruptHandler<peripherals::USB_OTG_FS>;
-// });
+bind_interrupts!(struct Irqs {
+    OTG_FS => usb_otg::InterruptHandler<peripherals::USB_OTG_FS>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = {
+        use embassy_stm32::rcc::*;
         let mut config: STM32Config = Default::default();
 
         config.enable_debug_during_sleep = false;
@@ -58,15 +58,15 @@ async fn main(_spawner: Spawner) {
         config.rcc.hsi = false;
         config.rcc.hse = Some(Hse {
             freq: Hertz::mhz(25),
-            mode: embassy_stm32::rcc::HseMode::Oscillator,
+            mode: HseMode::Oscillator,
         });
         config.rcc.sys = Sysclk::PLL1_P;
         config.rcc.pll_src = PllSource::HSE;
         config.rcc.pll = Some(Pll {
             prediv: PllPreDiv::DIV25,
-            mul: embassy_stm32::rcc::PllMul::MUL336,
-            divp: Some(embassy_stm32::rcc::PllPDiv::DIV4),
-            divq: Some(embassy_stm32::rcc::PllQDiv::DIV7),
+            mul: PllMul::MUL336,
+            divp: Some(PllPDiv::DIV4),
+            divq: Some(PllQDiv::DIV7),
             divr: None,
         });
         config.rcc.ahb_pre = AHBPrescaler::DIV1;
@@ -167,7 +167,7 @@ async fn main(_spawner: Spawner) {
     interrupt::USART2.set_priority(Priority::P6);
     let spawner = EXECUTOR_BLINK.start(interrupt::USART2);
     spawner.spawn(run_blink(led)).unwrap();
-    // spawner.spawn(run_usb(p.USB_OTG_FS, p.PA12, p.PA11)).unwrap();
+    spawner.spawn(run_usb(p.USB_OTG_FS, p.PA12, p.PA11)).unwrap();
 
     let executor = EXECUTOR_LS.init(Executor::new());
     executor.run(|s| {
@@ -661,89 +661,92 @@ async fn run_btn(mut button: ExtiInput<'static, PA1>, mut led: Output<'static, P
     }
 }
 
-// #[embassy_executor::task]
-// async fn run_usb(fs: USB_OTG_FS, pa12: PA12, pa11: PA11) {
-//     // Create the driver, from the HAL.
-//     let mut ep_out_buffer = [0u8; 256];
-//     let mut config = embassy_stm32::usb_otg::Config::default();
-//     config.vbus_detection = true;
-//     let driver = Driver::new_fs(fs, Irqs, pa12, pa11, &mut ep_out_buffer, config);
+#[embassy_executor::task]
+async fn run_usb(fs: USB_OTG_FS, pa12: PA12, pa11: PA11) {
+    // Create the driver, from the HAL.
+    let mut ep_out_buffer = [0u8; 256];
+    let mut config = embassy_stm32::usb_otg::Config::default();
+    config.vbus_detection = false;
+    let driver = Driver::new_fs(fs, Irqs, pa12, pa11, &mut ep_out_buffer, config);
 
-//     // Create embassy-usb Config
-//     let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
-//     config.manufacturer = Some("Embassy");
-//     config.product = Some("USB-serial example");
-//     config.serial_number = Some("12345678");
+    // Create embassy-usb Config
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("Embassy");
+    config.product = Some("USB-serial example");
+    config.serial_number = Some("12345678");
 
-//     // Required for windows compatibility.
-//     // https://developer.nordicsemi.com/nRF_Connect_SDK/doc/1.9.1/kconfig/CONFIG_CDC_ACM_IAD.html#help
-//     config.device_class = 0xEF;
-//     config.device_sub_class = 0x02;
-//     config.device_protocol = 0x01;
-//     config.composite_with_iads = true;
+    // Required for windows compatibility.
+    // https://developer.nordicsemi.com/nRF_Connect_SDK/doc/1.9.1/kconfig/CONFIG_CDC_ACM_IAD.html#help
+    config.device_class = 0xEF;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x01;
+    config.composite_with_iads = true;
 
-//     // Create embassy-usb DeviceBuilder using the driver and config.
-//     // It needs some buffers for building the descriptors.
-//     let mut device_descriptor = [0; 256];
-//     let mut config_descriptor = [0; 256];
-//     let mut bos_descriptor = [0; 256];
-//     let mut control_buf = [0; 64];
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut device_descriptor = [0; 256];
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
 
-//     let mut state = State::new();
+    let mut state = State::new();
 
-//     let mut builder = Builder::new(
-//         driver,
-//         config,
-//         &mut device_descriptor,
-//         &mut config_descriptor,
-//         &mut bos_descriptor,
-//         &mut [], // no msos descriptors
-//         &mut control_buf,
-//     );
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut device_descriptor,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut [], // no msos descriptors
+        &mut control_buf,
+    );
 
-//     // Create classes on the builder.
-//     let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
+    // Create classes on the builder.
+    let mut class = CdcAcmClass::new(&mut builder, &mut state, 64);
 
-//     // Build the builder.
-//     let mut usb = builder.build();
+    // Build the builder.
+    let mut usb = builder.build();
 
-//     // Run the USB device.
-//     let usb_fut = usb.run();
+    // Run the USB device.
+    let usb_fut = usb.run();
 
-//     // Do stuff with the class!
-//     let echo_fut = async {
-//         loop {
-//             class.wait_connection().await;
-//             info!("Connected");
-//             let _ = echo(&mut class).await;
-//             info!("Disconnected");
-//         }
-//     };
+    // Do stuff with the class!
+    let echo_fut = async {
+        loop {
+            class.wait_connection().await;
+            info!("Connected");
+            let _ = echo(&mut class).await;
+            info!("Disconnected");
+        }
+    };
 
-//     // Run everything concurrently.
-//     // If we had made everything `'static` above instead, we could do this using separate tasks instead.
-//     join(usb_fut, echo_fut).await;
-// }
+    // Run everything concurrently.
+    // If we had made everything `'static` above instead, we could do this using separate tasks instead.
+    join(usb_fut, echo_fut).await;
+}
 
-// struct Disconnected {}
+struct Disconnected {}
 
-// impl From<EndpointError> for Disconnected {
-//     fn from(val: EndpointError) -> Self {
-//         match val {
-//             EndpointError::BufferOverflow => Disconnected {},
-//             EndpointError::Disabled => Disconnected {},
-//         }
-//     }
-// }
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => Disconnected {},
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
 
-// async fn echo<'d, T: Instance + 'd>(
-//     class: &mut CdcAcmClass<'d, Driver<'d, T>>,
-// ) -> Result<(), Disconnected> {
-//     let mut buf = [0; 64];
-//     loop {
-//         let n = class.read_packet(&mut buf).await?;
-//         let data = &buf[..n];
-//         info!("data: {:x}", data);
-//         class.write_packet(data).await?;
-//     }
-// }
+async fn echo<'d, T: Instance + 'd>(
+    class: &mut CdcAcmClass<'d, Driver<'d, T>>,
+) -> Result<(), Disconnected> {
+    let mut buf = [0; 64];
+    loop {
+        let n = class.read_packet(&mut buf).await?;
+        for i in 0..n {
+            buf[i] += 1;
+        }
+        let data = &buf[..n];
+        info!("data: {:x}", data);
+        class.write_packet(data).await?;
+    }
+}
