d7355643d321d8e5ff35b94642950df5eb879e23
Glenn Dirkx <glenn.dirkx@gmail.com>
2026-03-19 17:21:24 +0100

    plex

--- modules/sshd.nix
+++ modules/sshd.nix
@@ -18,7 +18,7 @@
     port = 22;
   };
 
-  networking.firewall.allowedTCPPorts = [ 22 666 ];
-  networking.firewall.allowedUDPPorts = [ ];
+  networking.firewall.allowedTCPPorts = [ 22 666 32400 32410 32412 32413 32414 ];
+  networking.firewall.allowedUDPPorts = [ 32400 32410 32412 32413 32414 ];
   networking.firewall.enable = true;
 }
